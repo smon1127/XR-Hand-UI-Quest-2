ceca4f0aaffc9faae1965cd39bddb49c6f8770d3
Simon F <simon.fruebis@me.com>
2021-08-19 14:18:18 +0200

    Created targetObject definition for the last third of Object Count

--- Assets/Scripts/Randomizer.cs
+++ Assets/Scripts/Randomizer.cs
@@ -1,64 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Randomizer : MonoBehaviour
+﻿namespace Microsoft.MixedReality.Toolkit.Experimental.UI
 {
+    using Microsoft.MixedReality.Toolkit.UI;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Random = UnityEngine.Random;
 
-    public Transform gridObjectCollection; 
-    public List<Transform> targetObjectParent = new List<Transform>();
-    public List<Transform> targetObject = new List<Transform>();
-    private Transform tempGo;
-
-    public Transform gridObjectCollectionPanel;
-    public List<Transform> targetObjectParentPanel = new List<Transform>();
-    public List<Transform> targetObjectPanel = new List<Transform>();    
-    private Transform tempGoPanel;
-
-    void Start()
+    public class Randomizer : MonoBehaviour
     {
-        DeclareObjectOrder(gridObjectCollection, targetObjectParent, targetObject);
-        DeclareObjectOrder(gridObjectCollectionPanel, targetObjectParentPanel, targetObjectPanel);
-    }
 
-    public void DeclareObjectOrder(Transform _gridObjectCollection, List<Transform> _targetObjectParent, List<Transform> _targetObject)
-    {
-        int childCount = 0;
-        foreach (Transform child in _gridObjectCollection)
+        public Transform gridObjectCollection;
+        public List<Transform> targetObjectParent = new List<Transform>();
+        public List<Transform> targetObject = new List<Transform>();
+        private Transform tempGo;
+
+        public Transform gridObjectCollectionPanel;
+        public List<Transform> targetObjectParentPanel = new List<Transform>();
+        public List<Transform> targetObjectPanel = new List<Transform>();
+        private Transform tempGoPanel;
+        public ScrollingObjectCollection scrollObjectCollectionPanel;
+
+        public int currentTargetCount;
+
+        void Start()
         {
-            _targetObjectParent[childCount] = child;
-            _targetObject[childCount] = child.GetChild(0);
-            childCount++;
+            DeclareObjectOrder(gridObjectCollection, targetObjectParent, targetObject);
+            DeclareObjectOrder(gridObjectCollectionPanel, targetObjectParentPanel, targetObjectPanel);
         }
-    }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-            Shuffle();
-    } 
-
-    public void Shuffle()
-    {
-        for (int i = 0; i < targetObject.Count; i++)
+        public void DeclareObjectOrder(Transform _gridObjectCollection, List<Transform> _targetObjectParent, List<Transform> _targetObject)
         {
-            int rnd = Random.Range(i, targetObject.Count);
-            tempGo = targetObject[rnd];
-            targetObject[rnd] = targetObject[i];
-            targetObject[i] = tempGo;
+            int childCount = 0;
+            foreach (Transform child in _gridObjectCollection)
+            {
+                _targetObjectParent[childCount] = child;
+                _targetObject[childCount] = child.GetChild(0);
+                childCount++;
+            }
+        }
 
-            targetObject[i].SetParent(targetObjectParent[i]);
-            targetObject[i].localPosition = new Vector3(0,0,0);
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                Shuffle();
+        }
+
+        public void Shuffle()
+        {
+            for (int i = 0; i < targetObject.Count; i++)
+            {
+                int rnd = Random.Range(i, targetObject.Count);
+                tempGo = targetObject[rnd];
+                targetObject[rnd] = targetObject[i];
+                targetObject[i] = tempGo;
+
+                targetObject[i].SetParent(targetObjectParent[i]);
+                targetObject[i].localPosition = new Vector3(0, 0, 0);
 
 
+                tempGoPanel = targetObjectPanel[rnd];
+                targetObjectPanel[rnd] = targetObjectPanel[i];
+                targetObjectPanel[i] = tempGoPanel;
 
-            tempGoPanel = targetObjectPanel[rnd];
-            targetObjectPanel[rnd] = targetObjectPanel[i];
-            targetObjectPanel[i] = tempGoPanel;
+                targetObjectPanel[i].SetParent(targetObjectParentPanel[i]);
+                targetObjectPanel[i].localPosition = new Vector3(0, 0, 0);
+            }
+            //Define targetObject randomized of last third
+            currentTargetCount = Random.Range(targetObjectParent.Count / 3 * 2, targetObjectParent.Count);
 
-            targetObjectPanel[i].SetParent(targetObjectParentPanel[i]);
-            targetObjectPanel[i].localPosition = new Vector3(0, 0, 0);
-
+            //Scroll to y-Position with targetObject in Panel
+            float cellHeight = scrollObjectCollectionPanel.CellHeight;
+            Vector3 targetScrollPosition = new Vector3(0,currentTargetCount* cellHeight,0);
+            scrollObjectCollectionPanel.ApplyPosition(targetScrollPosition);
         }
     }
 }
