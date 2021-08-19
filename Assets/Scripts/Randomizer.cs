26a0738d50e80c84db2c30d073e8aeb7c08a8994
Simon F <simon.fruebis@me.com>
2021-08-19 13:28:26 +0200

    Created Randomizer for ScrollObjectCollection

---
+++ Assets/Scripts/Randomizer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomizer : MonoBehaviour
+{
+    public List<Transform> targetObjectParent = new List<Transform>();
+    public List<Transform> targetObject = new List<Transform>();
+    private Transform tempGO;
+    public Transform gridObjectCollection;
+    private Transform tempGo;
+
+    void Start()
+    {        
+        int childCount = 0;
+        foreach (Transform child in gridObjectCollection)
+        {
+            targetObjectParent[childCount] = child;
+            targetObject[childCount] = child.GetChild(0);
+            childCount++;
+        }  
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Shuffle();
+    }
+
+ 
+
+    public void Shuffle()
+    {
+        for (int i = 0; i < targetObject.Count; i++)
+        {
+            int rnd = Random.Range(i, targetObject.Count);
+            tempGo = targetObject[rnd];
+            targetObject[rnd] = targetObject[i];
+            targetObject[i] = tempGo;
+            targetObject[i].SetParent(targetObjectParent[i]);
+            targetObject[i].localPosition = new Vector3(0,0,0);
+        }
+    }
+}
