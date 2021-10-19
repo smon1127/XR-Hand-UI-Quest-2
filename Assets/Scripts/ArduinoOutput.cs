44497777ace4d24aa3155b41613c03eff854fe89
Simon F <simon.fruebis@me.com>
2021-10-19 20:08:05 +0200

    Successfully added Ardity for some Testings with TACHAMMER Arduino and Unity + MRTK

---
+++ Assets/Scripts/ArduinoOutput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//(2018) Made by Tyler Wargo
+
+public class ArduinoOutput : MonoBehaviour
+{
+	//Acces Serial Script
+	public ArduinoSerialConnect arduinoSerialConnect;
+
+	public void Start()
+	{
+		
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1)){
+			arduinoSerialConnect.serial.Write("A");
+		}
+
+		if (Input.GetKeyDown(KeyCode.Keypad2))
+		{
+			arduinoSerialConnect.serial.Write("B");
+		}
+	}
+}
---
+++ Assets/Scripts/ArduinoSerialConnect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+//(2018) Made by Tyler Wargo
+
+public class ArduinoSerialConnect : MonoBehaviour
+{
+	//Set Serial Port Data Stream
+	public SerialPort serial = new SerialPort("COM7", 9600);
+
+	public void Update()
+	{
+		//Check If Serial Data Stream Is Closed
+		if (!serial.IsOpen)
+			//Open Data Stream If Closed
+			serial.Open();
+	}
+}
