5984ff322f8222f004c8d3aa7535c38385981ea5
Simon F <simon.fruebis@me.com>
2021-12-22 01:06:02 +0100

    Adding controller mapping tool, Creating InputThimbalTest.cs

---
+++ Assets/Scripts/InputThimbalTest.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class InputThimbalTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log("button 1: " + Input.GetKey(KeyCode.JoystickButton1) + ", Axis 1: " + Input.GetAxis("AXIS_1"));
+            Debug.Log("button 2: " + Input.GetKey(KeyCode.JoystickButton2) + ", Axis 2: " + Input.GetAxis("AXIS_2"));
+            
+
+        }
+        
+    }
+}
