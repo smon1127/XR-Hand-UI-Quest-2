bfa43548d58a41b8dca70f79141c06daee51f37e
Simon F <simon.fruebis@me.com>
2021-12-28 02:15:16 +0100

    Found a solution to get Thimbel Data to MRTK Syntax

---
+++ Assets/Scripts/ThimbelDataProvider.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+
+public class ThimbelDataProvider : InputSystemGlobalHandlerListener, IMixedRealityInputHandler
+{
+
+    [Header("MRTK Input Actions")]
+    [SerializeField] private MixedRealityInputAction outputAction;
+
+
+    [SerializeField] private bool thimbelTouched;
+
+    public MixedRealityInputAction inputAction;
+
+    private void Update()
+    {
+        foreach (var controller in CoreServices.InputSystem.DetectedControllers)
+        {
+            if (controller.InputSource.SourceType == InputSourceType.Controller)
+            {
+                if (Input.GetKey(KeyCode.JoystickButton0) || Input.GetKey(KeyCode.JoystickButton1) || Input.GetKey(KeyCode.JoystickButton2))
+                {
+                    CoreServices.InputSystem?.RaiseOnInputDown(controller.InputSource, Handedness.Any, inputAction);
+                }
+                else
+                {
+                    CoreServices.InputSystem?.RaiseOnInputUp(controller.InputSource, Handedness.Any, inputAction);
+                }
+            }
+        }
+    }
+
+    #region On Input Up/Down
+    public void OnInputUp(InputEventData eventData)
+    {
+        if (eventData.MixedRealityInputAction == outputAction) thimbelTouched = false;
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        if (eventData.MixedRealityInputAction == outputAction) thimbelTouched = true;
+    }
+    #endregion
+
+    #region Handlers
+    protected override void RegisterHandlers()
+    {
+        CoreServices.InputSystem.RegisterHandler<IMixedRealityInputHandler>(this);
+    }
+
+    protected override void UnregisterHandlers()
+    {
+
+    }
+    #endregion
+}
---
+++ Assets/Scripts/ThimbelTest.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+
+public class ThimbelTest : MonoBehaviour
+{    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log("button 1: " + Input.GetKey(KeyCode.JoystickButton1) + ", Axis 1: " + Input.GetAxis("AXIS_1"));
+            Debug.Log("button 2: " + Input.GetKey(KeyCode.JoystickButton2) + ", Axis 2: " + Input.GetAxis("AXIS_2"));           
+
+        }
+    }
+}
