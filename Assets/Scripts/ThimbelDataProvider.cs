f7c46c5e467165788d0e72d43488877b9c853ccc
Simon F <simon.fruebis@me.com>
2022-01-08 22:58:21 +0100

    Fixed thimbel toggle bug by changing from bool type to enum with flags.

--- Assets/Scripts/ThimbelDataProvider.cs
+++ Assets/Scripts/ThimbelDataProvider.cs
@@ -2,25 +2,148 @@
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections.Generic;
+using System;
 
 public class ThimbelDataProvider : InputSystemGlobalHandlerListener, IMixedRealityInputHandler
 {
 
     [Header("MRTK Input Actions")]
     [SerializeField] private MixedRealityInputAction outputAction;
-
-
-    [SerializeField] private bool thimbelTouched;
-
     public MixedRealityInputAction inputAction;
 
+    [SerializeField] private bool thimbelTouched = false;
+
+
+    public KeyCode[] btn = new KeyCode[] {KeyCode.JoystickButton0, KeyCode.JoystickButton1, KeyCode.JoystickButton2};
+
+    [Flags]public enum ActiveThimbel
+    {
+        thimbel0 = 1,
+        thimbel1 = 2,
+        thimbel2 = 4,
+        //thimbel0AndThimbel1 = thimbel0 | thimbel1
+    }
+    public ActiveThimbel activeThimbel;
+
+   
+
     private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            activeThimbel ^= ActiveThimbel.thimbel0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            activeThimbel ^= ActiveThimbel.thimbel1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            activeThimbel ^= ActiveThimbel.thimbel2;
+        }
+
+
+
+        // Don´t get confused. (int)activeThimble displays the flag value. Check following Debug to understand.
+        Debug.Log("activeThimbel: " + (int)activeThimbel);
+
+        if ((int)activeThimbel == 7)
+        {
+            if (Input.GetKey(btn[0]) || Input.GetKey(btn[1]) || Input.GetKey(btn[2]))
+            {
+                ActivateButton(true);
+            }
+            else
+            {
+                ActivateButton(false);
+            }
+
+        }else if ((int)activeThimbel == 3)
+        {
+            if(Input.GetKey(btn[0]) || Input.GetKey(btn[1]))
+            {
+                ActivateButton(true);
+            }
+            else
+            {
+                ActivateButton(false);
+            }
+                
+        }else if ((int)activeThimbel == 6)
+        {
+            if (Input.GetKey(btn[1]) || Input.GetKey(btn[2]))
+            {
+                ActivateButton(true);
+            }
+            else
+            {
+                ActivateButton(false);
+            }
+
+        }else if ((int)activeThimbel == 5)
+        {
+            if (Input.GetKey(btn[0]) || Input.GetKey(btn[2]))
+            {
+                ActivateButton(true);
+            }
+            else
+            {
+                ActivateButton(false);
+            }
+
+        }else if ((int)activeThimbel == 1)
+        {
+            if (Input.GetKey(btn[0]))
+            {
+                ActivateButton(true);
+            }
+            else
+            {
+                ActivateButton(false);
+            }
+
+        }else if ((int)activeThimbel == 2)
+        {
+            if (Input.GetKey(btn[1]))
+            {
+                ActivateButton(true);
+            }
+            else
+            {
+                ActivateButton(false);
+            }
+
+        }else if ((int)activeThimbel == 4)
+        {
+            if (Input.GetKey(btn[2]))
+            {
+                ActivateButton(true);
+            }
+            else
+            {
+                ActivateButton(false);
+            }
+        }else
+        {
+            ActivateButton(false);
+        }
+
+
+        
+    }
+
+    public void ActivateButton(bool isActive)
     {
         foreach (var controller in CoreServices.InputSystem.DetectedControllers)
         {
             if (controller.InputSource.SourceType == InputSourceType.Controller)
             {
-                if (Input.GetKey(KeyCode.JoystickButton0) || Input.GetKey(KeyCode.JoystickButton1) || Input.GetKey(KeyCode.JoystickButton2))
+
+                if (isActive)
                 {
                     CoreServices.InputSystem?.RaiseOnInputDown(controller.InputSource, Handedness.Any, inputAction);
                 }
